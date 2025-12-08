878efce2a533cb6a010ad9bb3dc46fbd03fb40c7
vansh-09 <vanshkjain09@gmail.com>
2025-12-08 22:52:26 +0530

    deployment bugs

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,10 +27,10 @@ app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
 
